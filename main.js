a4d151cb818df3b45f1792747bb4a6d8f6a2a1e0
tu-nguyen <valcrune@gmail.com>
2021-01-15 11:32:00 -0500

    starting to test core function of bot, gonna be rough spaghetti I'm sorry for anyone that sees

--- main.js
+++ main.js
@@ -12,28 +12,54 @@ const tmiClient = new tmi.Client({
 	},
 	channels: ['jt1gaming', 'ascidgaming']
 });
+
+// ripped from https://github.com/mangosango/clive
+// (twitch.tv\/.*\/clip) check https://www.twitch.tv/username/clip/clip_id
+// (clips.twitch.tv) checks https://clips.twitch.tv/clip_id
+const CLIPS_REGEX = /(twitch.tv\/.*\/clip)|(clips.twitch.tv)\/\w+/i;
+
 tmiClient.connect().catch(console.error);
-tmiClient.on('message', (channel, tags, message, self) => {
+
+tmiClient.on('message', (channel, userstate, message, self) => {
     if(self) return;
     console.log(message);
     console.log(tags);
 	if(message.toLowerCase() === '!hello') {
-		tmiClient.say(channel, `@${tags.username}, heya!`);
+		tmiClient.say(channel, `@${userstate.username}, heya!`);
 	}
+
+	checkForClips(userstate, message)
+
 });
 
-const Discord = require('discord.js');
+function checkForClips(username, message){
+	let isClip = false
+	isClip = CLIPS_REGEX.test(message);
 
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+	if(isClip) {
+		tmiClient.say(channel, `clip detected`);
+	}
 
-const discordClient = new Discord.Client();
+}
+
+function postToDiscord(){
+
+}
 
 
-discordClient.once('ready', () => {
-    console.log('burrito-guy is online!');
-});
+
+// const Discord = require('discord.js');
+
+// const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+// const discordClient = new Discord.Client();
+
+
+// discordClient.once('ready', () => {
+//     console.log('burrito-guy is online!');
+// });
 
 
 
-discordClient.login(DISCORD_TOKEN);
+// discordClient.login(DISCORD_TOKEN);
 
